6934c8d026b2d21e6b121ea30ae1ff5ae2755abc
Utkarsh <mahai.utkarsh@gmail.com>
2017-03-29 15:22:45 +0530

    Create firstProg.java

---
+++ firstProg.java
@@ -0,0 +1,13 @@
+public class firstProg {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+/*		System.out.println("Hello");
+*/	
+		System.out.println("Bye");
+		System.out.println("Bye2");
+		System.out.println("Bye3");
+System.out.println("Bye4");
+		}
+
+}
